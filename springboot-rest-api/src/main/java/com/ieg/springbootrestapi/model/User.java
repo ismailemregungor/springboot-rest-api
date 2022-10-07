810ae5fbb511ba1c8432fd449e095977810716ee
ismailemregungor <ismailemre.gungor@gazi.edu.tr>
2022-10-08 02:21:18 +0300

    Create User.java

---
+++ springboot-rest-api/src/main/java/com/ieg/springbootrestapi/model/User.java
@@ -0,0 +1,25 @@
+package com.ieg.springbootrestapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 50)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 50)
+    private String lastName;
+
+}
