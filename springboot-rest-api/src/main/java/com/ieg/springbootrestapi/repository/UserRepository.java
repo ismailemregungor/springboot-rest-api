f8df4c5730ece35c9210560593d66b15411521d6
ismailemregungor <ismailemre.gungor@gazi.edu.tr>
2022-10-08 16:57:57 +0300

    Update UserRepository.java

--- springboot-rest-api/src/main/java/com/ieg/springbootrestapi/repository/UserRepository.java
+++ springboot-rest-api/src/main/java/com/ieg/springbootrestapi/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.ieg.springbootrestapi.repository;
 
 import com.ieg.springbootrestapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
