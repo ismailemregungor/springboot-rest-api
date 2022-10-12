43d8f42e766df2f2020d7a839b126a0acbea9776
İsmail Emre Güngör <ismail.gungor@d-teknoloji.com.tr>
2022-10-13 01:44:09 +0300

    Create UserUpdateDTO.java

---
+++ springboot-rest-api/src/main/java/com/ieg/springbootrestapi/dto/UserUpdateDTO.java
@@ -0,0 +1,12 @@
+package com.ieg.springbootrestapi.dto;
+
+import lombok.Data;
+
+@Data
+public class UserUpdateDTO {
+
+    private final String firstName;
+
+    private final String lastName;
+
+}
