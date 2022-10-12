5c24b32417b02f97a10885e69ed6f167b04eac9f
İsmail Emre Güngör <ismail.gungor@d-teknoloji.com.tr>
2022-10-13 01:43:32 +0300

    Create UserAddDTO.java

---
+++ springboot-rest-api/src/main/java/com/ieg/springbootrestapi/dto/UserAddDTO.java
@@ -0,0 +1,11 @@
+package com.ieg.springbootrestapi.dto;
+
+import lombok.Data;
+@Data
+public final class UserAddDTO {
+
+    private final String firstName;
+
+    private final String lastName;
+
+}
