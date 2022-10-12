eedde5a1216ac8456703068fa694cc689d42b2b1
İsmail Emre Güngör <ismail.gungor@d-teknoloji.com.tr>
2022-10-13 01:43:12 +0300

    Create GenericResponse.java

---
+++ springboot-rest-api/src/main/java/com/ieg/springbootrestapi/shared/GenericResponse.java
@@ -0,0 +1,13 @@
+package com.ieg.springbootrestapi.shared;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@AllArgsConstructor
+public class GenericResponse {
+
+    private String message;
+
+}
