ae9b2de698865a67ce1dd163c92f51bcb32d10ba
İsmail Emre Güngör <ismail.gungor@d-teknoloji.com.tr>
2022-10-13 01:44:28 +0300

    Update UserService.java

--- springboot-rest-api/src/main/java/com/ieg/springbootrestapi/service/UserService.java
+++ springboot-rest-api/src/main/java/com/ieg/springbootrestapi/service/UserService.java
@@ -1,7 +1,15 @@
 package com.ieg.springbootrestapi.service;
 
-import com.ieg.springbootrestapi.dto.UserViewDTO;
+import com.ieg.springbootrestapi.dto.UserAddDTO;
+import com.ieg.springbootrestapi.dto.UserGetDTO;
+import com.ieg.springbootrestapi.dto.UserUpdateDTO;
+
+import java.util.List;
 public interface UserService {
-    UserViewDTO getUserById(Long id);
+    UserGetDTO add(UserAddDTO userAddDTO);
+    UserGetDTO update(Long id, UserUpdateDTO userUpdateDTO);
+    void delete(Long id);
+    List<UserGetDTO> getAll();
+    UserGetDTO getUserById(Long id);
 
 }
